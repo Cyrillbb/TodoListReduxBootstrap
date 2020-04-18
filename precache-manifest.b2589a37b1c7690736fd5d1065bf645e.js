d15586f65a715dc8194aa9b3cc6af5ed5594cf53
Fallopio <kibaryshnikov@yandex.ru>
2020-04-18 19:56:11 +0400

    Updates

---
+++ precache-manifest.b2589a37b1c7690736fd5d1065bf645e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8455882bde7f85fced46ae05837b23",
+    "url": "/TodoListReduxBootstrap/index.html"
+  },
+  {
+    "revision": "a5ec95f23ca0cbda4b42",
+    "url": "/TodoListReduxBootstrap/static/css/main.70838d6a.chunk.css"
+  },
+  {
+    "revision": "17e4ffb3c5e1dce497e2",
+    "url": "/TodoListReduxBootstrap/static/js/2.28468c9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoListReduxBootstrap/static/js/2.28468c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ec95f23ca0cbda4b42",
+    "url": "/TodoListReduxBootstrap/static/js/main.b970ea62.chunk.js"
+  },
+  {
+    "revision": "a0fefc7879efb769e507",
+    "url": "/TodoListReduxBootstrap/static/js/runtime-main.985d7ccf.js"
+  }
+]);
